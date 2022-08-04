6002bafeca6c507df750961607e59fa10ef6222d
dzulix <dzulix@outlook.com>
2022-08-04 11:49:12 +0200

    MTTL-3132: Add feature toggle.

--- src/root.tsx
+++ src/root.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 
 import { ConfirmationRouter } from "@mtfh/common/lib/components";
+import { useFeatureToggle } from "@mtfh/common/lib/hooks";
 
 import App from "./app";
 
 const Root = (): JSX.Element => {
-  return (
+  const hasWorktray = useFeatureToggle("MMH.Worktray");
+
+  return hasWorktray ? (
     <ConfirmationRouter>
       <App />
     </ConfirmationRouter>
+  ) : (
+    <div />
   );
 };
 
