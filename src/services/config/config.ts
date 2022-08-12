0dc1ed1856846a64a4119f4daaab17f00ab35aa8
dzulix <--local>
2022-08-12 15:16:18 +0200

    MTTL-3138: Add mtfh/processes and update configs.

--- src/services/config/config.ts
+++ src/services/config/config.ts
@@ -1,4 +1,3 @@
 export const config = {
-  worktrayApiUrl:
-    "https://9d522ffa-5f9a-48cc-8eaf-f23add75fccc.mock.pstmn.io" || "/worktray-api",
+  worktrayApiUrl: process.env.WORKTRAY_API_URL || "/api/worktray",
 };
