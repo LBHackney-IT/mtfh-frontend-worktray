c4b2ac4b57cbdd0333753c1da8ac26fb724740c3
Julia Wojciechowska <dzulix@outlook.com>
2022-10-12 08:41:54 +0200

    MTTL-3415: Populate filters with real data. (#12)
    * MTTL-3415: Populate filters with real data.
    
    * MTTL-3415: Fix tests.

--- src/components/worktray-filters/worktray-filters.test.tsx
+++ src/components/worktray-filters/worktray-filters.test.tsx
@@ -3,32 +3,13 @@ import React from "react";
 import { render } from "@hackney/mtfh-test-utils";
 import { fireEvent, screen } from "@testing-library/react";
 
+import { processes } from "@mtfh/processes";
+
 import { WorktrayURLProvider } from "../../context/worktray-context";
 import { locale } from "../../services";
 import { WorktrayFilters } from "./worktray-filters";
 
 const { components } = locale;
-
-const filters = [
-  {
-    type: "processes",
-    title: "Processes",
-    options: [
-      { key: "process-1", value: "Process 1" },
-      { key: "process-2", value: "Process 2" },
-      { key: "process-3", value: "Process 3" },
-    ],
-  },
-  {
-    type: "patches",
-    title: "Patches",
-    options: [
-      { key: "cp1", value: "CP1" },
-      { key: "cp2", value: "CP2" },
-      { key: "cp3", value: "CP3" },
-    ],
-  },
-];
 
 describe("worktray-filters", () => {
   beforeEach(() => {
@@ -36,8 +17,13 @@ describe("worktray-filters", () => {
   });
 
   test("it renders WorktrayFilters correctly", async () => {
-    const { container } = render(<WorktrayFilters />);
-    expect(container).toMatchSnapshot();
+    render(<WorktrayFilters />);
+    await expect(
+      screen.findByText(processes.soletojoint.name),
+    ).resolves.toBeInTheDocument();
+    await expect(
+      screen.findByText("Awaiting Housing Officer review"),
+    ).resolves.toBeInTheDocument();
   });
 
   test("it selects and unselects filter options", async () => {
@@ -46,24 +32,24 @@ describe("worktray-filters", () => {
         <WorktrayFilters />
       </WorktrayURLProvider>,
       {
-        url: "/?t=30&sort=status&process=%2CProcess+2&patch=%2CCP1",
+        url: "/?t=30&sort=status&process=%2Cchange-of-name&patch=%2CCP1",
         path: "/",
       },
     );
     await expect(
-      screen.findByLabelText(filters[0].options[0].value),
+      screen.findByLabelText(processes.soletojoint.name),
     ).resolves.not.toBeChecked();
-    fireEvent.click(screen.getByLabelText(filters[0].options[0].value));
+    fireEvent.click(screen.getByLabelText(processes.soletojoint.name));
     await expect(
-      screen.findByLabelText(filters[0].options[0].value),
+      screen.findByLabelText(processes.soletojoint.name),
     ).resolves.toBeChecked();
     fireEvent.click(screen.getByText(locale.components.filters.applyFilters));
     expect(window.sessionStorage.getItem("test")).toBe(
-      "?t=30&sort=status&patch=%2CCP1&process=%2CProcess+2%2Cprocess-1",
+      "?t=30&sort=status&patch=%2CCP1&process=%2Cchange-of-name%2Csole-to-joint",
     );
-    fireEvent.click(screen.getByLabelText(filters[0].options[0].value));
+    fireEvent.click(screen.getByLabelText(processes.soletojoint.name));
     await expect(
-      screen.findByLabelText(filters[0].options[0].value),
+      screen.findByLabelText(processes.soletojoint.name),
     ).resolves.not.toBeChecked();
   });
 
@@ -75,37 +61,28 @@ describe("worktray-filters", () => {
     );
 
     await expect(
-      screen.findByLabelText(filters[0].options[0].value),
+      screen.findByLabelText(processes.soletojoint.name),
     ).resolves.not.toBeChecked();
     await expect(
-      screen.findByLabelText(filters[0].options[1].value),
-    ).resolves.not.toBeChecked();
-    await expect(
-      screen.findByLabelText(filters[0].options[2].value),
+      screen.findByLabelText(processes.changeofname.name),
     ).resolves.not.toBeChecked();
 
     fireEvent.click(screen.getAllByText(components.filters.selectAll)[0]);
 
     await expect(
-      screen.findByLabelText(filters[0].options[0].value),
+      screen.findByLabelText(processes.soletojoint.name),
     ).resolves.toBeChecked();
     await expect(
-      screen.findByLabelText(filters[0].options[1].value),
-    ).resolves.toBeChecked();
-    await expect(
-      screen.findByLabelText(filters[0].options[2].value),
+      screen.findByLabelText(processes.changeofname.name),
     ).resolves.toBeChecked();
 
     fireEvent.click(screen.getByText(components.filters.clearFilters));
 
     await expect(
-      screen.findByLabelText(filters[0].options[0].value),
+      screen.findByLabelText(processes.soletojoint.name),
     ).resolves.not.toBeChecked();
     await expect(
-      screen.findByLabelText(filters[0].options[1].value),
-    ).resolves.not.toBeChecked();
-    await expect(
-      screen.findByLabelText(filters[0].options[2].value),
+      screen.findByLabelText(processes.changeofname.name),
     ).resolves.not.toBeChecked();
   });
 });
